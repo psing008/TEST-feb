d619c6a87d8dfd0a23e17d87ca9c0e0ead45df10
Piyush Singh <psing007@ucr.edu>
2013-04-11 22:07:26 -0700

    3rd

--- app/assets/javascripts/raffle.js.coffee
+++
@@ -1,9 +0,0 @@
-@RaffleCtrl = ($scope) ->
-	$scope.entries = [ 
-
-{name: "sam"}
-{name: "larry"}
-{name: "aka"}
-
-
-	]
--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Raffler::Application.config.secret_token = '9133a3e682929048dd08abf8534f3c248a60a39d1141b32033e39b688aed8bb04c20411d42fb0424826b85ce3d129bebbad36412b279ef2faef18665b911e741'
